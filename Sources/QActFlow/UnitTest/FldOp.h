4d747ae94a1154357790508d2b61d4aa7c93d2c5
test5 <test5@163.com>
2023-10-16 20:20:57 +0800

    part of unittest

---
+++ Sources/QActFlow/UnitTest/FldOp.h
@@ -0,0 +1,133 @@
+#ifndef FLDOP_H_
+#define FLDOP_H_
+ 
+#include <device_launch_parameters.h>
+#include <cufft.h>
+#include <cuComplex.h>
+#include "cuComplexBinOp.h"
+// #include "cudaErr.h"
+struct Mesh;
+struct field;
+struct param;
+
+/*******************************************************************
+General functions
+*********************************************************************/
+inline void FwdTrans(Mesh* pmesh, float* phys, cuComplex* spec);
+inline void BwdTrans(Mesh* pmesh, cuComplex* spec, float* phys);
+__global__ void coeff(float *f, int Nx, int Ny);
+
+// __Device: phys field multiplication: pc(x) = C*pa(x,y)*pb(x,y). prefix p denotes physical
+__global__ void FldMul(float* pa, float* pb, float C, float* pc, int Nx, int Ny);
+
+// __Device: phys field addition: pc(x,y) = a*pa(x,y) + b*pb(x,y). prefix p denotes physical
+__global__  void FldAdd(float* pa, float* pb, float a, float b, float* pc, int Nx, int Ny);
+__global__  void FldAdd(float a, float* pa, float b, float* pb, float* pc, int Nx, int Ny);
+
+// divide a factor after transforming from spectral to physical
+__global__ void coeff(float *f, int Nx, int Ny);
+
+__global__
+// set physical field equals to a constant field
+void FldSet(float * pf, float c, int Nx, int Ny);
+
+__global__
+// set two physical field equals
+void FldSet(float * pf, float* c, int Nx, int Ny);
+
+__global__
+void SpecSet(cuComplex * pa, cuComplex* pb, int Nxh, int Ny);
+
+__global__ 
+// spectral addition: spc(k,l) = a*spa(k,l) + b*spb(k,l)
+void SpecAdd(cuComplex* spa, cuComplex* spb, float a, float b, cuComplex* spc, int Nxh, int Ny);
+
+__global__
+void SpecAdd(float a, cuComplex* spa, float b, cuComplex* spb, cuComplex* spc, int Nxh, int Ny);
+
+__global__ 
+void xDerivD(cuComplex *ft, cuComplex *dft, float* kx, int Nxh, int Ny);
+inline void xDeriv(cuComplex *ft, cuComplex *dft, Mesh *mesh);
+
+__global__ 
+void yDerivD(cuComplex *ft, cuComplex *dft, float* ky, int Nxh, int Ny);
+inline void yDeriv(cuComplex *ft, cuComplex *dft, Mesh *mesh);
+
+// maintain the symmetry on y wave axis
+__global__ 
+void reality_func(cuComplex *spec, int Nxh, int Ny);
+
+__global__ 
+void laplacian_funcD(cuComplex *ft, cuComplex *lft, int Nxh, int Ny, float* k_squared);
+inline void laplacian_func(cuComplex *ft, cuComplex *lft, Mesh* mesh);
+
+__global__ 
+void vel_funcD(cuComplex* w_spec, cuComplex* u_spec, cuComplex* v_spec, 
+                            float* k_squared, float* kx, float*ky, int Nxh, int Ny);
+inline void vel_func(field w, field u, field v);
+
+// 4 steps of RK4 under spectral linear factor trick 
+__global__
+void integrate_func0(cuComplex* spec_old, cuComplex* spec_curr, cuComplex* spec_new,
+                    float* IF, float* IFh, int Nxh, int Ny, float dt);
+
+__global__  
+void integrate_func1(cuComplex* spec_old, cuComplex* spec_curr, cuComplex* spec_new, cuComplex* spec_nonl,
+                    float* IF, float* IFh, int Nxh, int Ny, float dt);
+
+__global__ 
+void integrate_func2(cuComplex* spec_old, cuComplex* spec_curr, cuComplex* spec_new, 
+                        cuComplex* spec_nonl,float* IF, float* IFh, int Nxh, int Ny, float dt);
+
+__global__ 
+void integrate_func3(cuComplex* spec_old, cuComplex* spec_curr, cuComplex* spec_new, 
+                        cuComplex* spec_nonl,float* IF, float* IFh, int Nxh, int Ny, float dt);
+
+__global__ 
+void integrate_func4(cuComplex* spec_old, cuComplex* spec_curr, cuComplex* spec_new, 
+                        cuComplex* spec_nonl,float* IF, float* IFh, int Nxh, int Ny, float dt);
+
+/*******************************************************************
+specialized functions
+*********************************************************************/
+__global__ 
+void S_func(float* r1, float*r2, float* S, int Nx, int Ny);
+
+void curr_func(field r1curr, field r2curr, field wcurr, field u, field v, field S);
+
+inline void r1nonl_func(field r1nonl, field r1nonl_appr, field r1, field r2, field w, 
+                        field u, field v, field S, float lambda, float cn, float Pe);
+
+inline void r2nonl_func(field r2nonl, field r2nonl_appr, field r1, field r2, field w, 
+                        field u, field v, field S, float lambda, float cn, float Pe);
+
+inline void wnonl_func(field wnonl, field wnonl_appr, field appr1, field p11, field p12, field p21, field r1, field r2, field w, 
+                        field u, field v, field alpha, field S, float Re, float Er, float cn, float lambda);
+
+inline void pCross_func(field p,field appr, field r1, field r2);
+
+inline void pSingle_func(field p, field appr, field r, field S, field alpha, float lambda, float cn);
+
+inline void p11nonl_func(field p11, field appr, field appr1, field r1, field r2, field S, 
+                        field alpha, float lambda, float cn);
+
+inline void p12nonl_func(field p12, field appr, field appr1, field r1, field r2, field S, 
+                        field alpha, float lambda, float cn);
+
+inline void p21nonl_func(field p21, field appr, field appr1, field r1, field r2, field S, 
+                        field alpha, float lambda, float cn);
+
+__global__
+void precompute_funcD(field r1, field r2, field w, field alpha, int Nx, int Ny);
+
+__global__
+void r1lin_func(float* IFr1h, float* IFr1, float* k_squared, float Pe, float cn, float dt);
+
+__global__
+void r2lin_func(float* IFr2h, float* IFr2, float* k_squared, float Pe, float cn, float dt);
+
+__global__
+void wlin_func(float* IFr1h, float* IFr1, float* k_squared, float Re, float cf, float dt);
+
+
+#endif
---
+++ Sources/QActFlow/UnitTest/c2r/c2rtest.cu
@@ -0,0 +1,252 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+#include <chrono>
+#include <random>
+
+#include <cufft.h>
+#include <cuComplex.h>
+#include "cuComplexBinOp.h"
+#include "cudaErr.h"
+
+#define M_PI 3.14159265358979323846
+// amount of kernels called, block_size must be n*32
+#define BSZ 4
+
+using namespace std;
+int Nx = 16;
+int Ny = 16;
+int Nxh = Nx/2+1;
+float Lx = 2*M_PI;
+float Ly = 2*M_PI;
+float dx = 2*M_PI/Nx;
+float dy = 2*M_PI/Ny;
+dim3 dimGrid  (int((Nx-0.5)/BSZ) + 1, int((Ny-0.5)/BSZ) + 1);
+dim3 dimBlock (BSZ, BSZ); 
+
+typedef struct session{
+    int Nx;
+    int Ny;
+    float Lx;
+    float Ly;
+    
+    float dx;
+    float dy;
+
+    float *kx;
+    float *ky;
+    
+    float alphax;
+    float alphay;
+
+    session(int Nx, int Ny, float Lx, float Ly):Nx(Nx), Ny(Ny), Lx(Lx), 
+    Ly(Ly),dx(2*M_PI/Nx), dy(2*M_PI/Ny),alphax(2*M_PI/Lx),alphay(2*M_PI/Ly){
+
+        cuda_error_func(cudaMallocManaged( &(this->kx), sizeof(float)*(Nx)));
+        cuda_error_func(cudaMallocManaged( &(this->ky), sizeof(float)*(Ny)));
+        for (int i=0; i<Nx; i++)          
+        {
+            if (i<=Nx/2+1)
+            this->kx[i] = i*2*M_PI/this->alphax;
+            else 
+            this->kx[i] = (i-Nx)*2*M_PI/this->alphax;
+        } 
+        for (int j=0; j<Ny; j++)          
+        {
+            if(j<+Nx/2+1)
+            this->ky[j] = j*2*M_PI/this->alphay;
+            else 
+            this->ky[j] = (j-Ny)*2*M_PI/this->alphay;
+        } 
+    }
+} session;
+
+typedef struct field{
+    session* mesh;
+    float* phys;
+    cuComplex* spec;
+    field(session* Mesh):mesh(Mesh){
+        cuda_error_func(cudaMallocManaged(&(this->phys), sizeof(float)*((mesh->Nx)*(mesh->Ny))));
+        cuda_error_func(cudaMallocManaged(&(this->spec), sizeof(cuComplex)*((mesh->Ny)*(mesh->Nx/2+1))));
+    }
+}field;
+
+__global__ void D_exact(float *t, int Nx, int Ny, float dx, float dy){
+    int i = blockIdx.x * BSZ + threadIdx.x;
+    int j = blockIdx.y * BSZ + threadIdx.y;
+    int index = j*Nx + i;
+    if(i < Nx && j < Ny){
+        t[index] = cos((float)j*dy);
+    }
+}
+
+// void exact(float *t, session *s){
+//     D_exact<<<dimGrid, dimBlock>>>(t, s->Nx, s->Ny, s->dx, s->dy);
+// }
+
+__global__ void D_init(float *t, int Nx, int Ny, float dx, float dy){
+    int i = blockIdx.x * BSZ + threadIdx.x;
+    int j = blockIdx.y * BSZ + threadIdx.y;
+    int index = j*Nx + i;
+    if(i < Nx && j < Ny){
+        t[index] = 5*sin((float)j*dy) + 3*cos((float)j*dy) - 2*sin((float)i*dx);
+    }
+}
+
+inline void init(float *t){
+    D_init<<<dimGrid, dimBlock>>>(t,Nx,Ny,dx,dy);
+    cuda_error_func( cudaPeekAtLastError() );
+	cuda_error_func( cudaDeviceSynchronize() );
+}
+
+
+void print_float(float* t, int Nx, int Ny) {
+    for (int j = 0; j < Ny; j++) {
+        for (int i = 0; i < Nx; i++) {
+            cout <<t[j*Nx+i] << ",";
+        }
+        cout << endl;
+    }
+}
+
+void exact(float *t, int Nx, int Ny, float dx,float dy){
+    D_exact<<<dimGrid, dimBlock>>>(t, Nx, Ny, dx, dy);
+    cuda_error_func( cudaPeekAtLastError() );
+	cuda_error_func( cudaDeviceSynchronize() );
+}
+
+__global__ void Dx(cuComplex *ft, cuComplex *dft, float* kx, int Nxh, int Ny){
+    int i = blockIdx.x * BSZ + threadIdx.x;
+    int j = blockIdx.y * BSZ + threadIdx.y;
+    int index = j*Nxh + i;
+
+    if(i<Nxh && j<Ny){
+        if(i==0 && j==0){
+            dft[index].x = 0;
+            dft[index].y = 0;
+        }
+        else{
+            dft[index] = ft[index]*im()*kx[i];
+        }
+    }
+}
+
+__global__ void Dy(cuComplex *ft, cuComplex *dft, float* ky, int Nxh, int Ny){
+    int i = blockIdx.x * BSZ + threadIdx.x;
+    int j = blockIdx.y * BSZ + threadIdx.y;
+    int index = j*Nxh + i;
+    if(i<Nxh && j<Ny){
+        if(i==0 && j==0){
+            dft[index].x = 0;
+            dft[index].y = 0;
+        }
+        else{
+            dft[index] = dft[index] = ft[index]*im()*ky[j];
+        }
+    }
+}
+__global__ void coeff(float *f, int Nx, int Ny){
+    int i = blockIdx.x * BSZ + threadIdx.x;
+    int j = blockIdx.y * BSZ + threadIdx.y;
+    int index = j*Nx + i;
+    if(i<Nx && j<Ny){
+        f[index] = f[index]/(Nx*Ny);
+    }
+}
+inline void FwdTrans(cufftHandle transf, float* f, cuComplex* ft, int Nx, int Ny){
+    cufft_error_func( cufftExecR2C(transf, f, ft));
+}
+inline void BwdTrans(cufftHandle inv_transf, cuComplex* ft, float* f, int Nx, int Ny){
+    cufft_error_func( cufftExecC2R(inv_transf, ft, f));
+    coeff<<<dimGrid, dimBlock>>>(f, Nx, Ny);
+}
+int main(){
+    float *test;
+    cuComplex* htest;
+    cuComplex* tmp;
+    float *kx;
+    float *ky;
+    float alpha = 2* M_PI;
+    float* exa;
+    cuda_error_func(cudaMallocManaged( &test, sizeof(float)*(Nx*Ny) ) );
+    cuda_error_func(cudaMallocManaged( &htest, sizeof(cuComplex)*(Nxh*Ny) ) );
+    cuda_error_func(cudaMallocManaged( &tmp, sizeof(cuComplex)*(Nxh*Ny) ) );
+    cuda_error_func(cudaMallocManaged( &exa, sizeof(float)*(Nx*Ny) ) );
+    cuda_error_func(cudaMallocManaged( &kx, sizeof(cuComplex)*(Nxh) ) );
+    cuda_error_func(cudaMallocManaged( &ky, sizeof(cuComplex)*(Ny) ) );
+    cuda_error_func( cudaPeekAtLastError() );
+	cuda_error_func( cudaDeviceSynchronize() );
+    for(int i = 0; i < Nxh; i++)
+    {
+        kx[i] = 1 * i;
+    }
+    
+    for (int i=0; i<=Ny/2; i++)          
+	{
+	   ky[i] = i*2*M_PI/alpha;
+    } 
+	for (int i=Ny/2+1; i<Ny; i++)          
+	{
+        ky[i] = (i - Ny) * 1;
+	}
+
+    init(test);
+    cout << "input: " << endl;
+    print_float(test,Nx,Ny);
+    exact(exa,Nx,Ny,dx,dy);
+    cout << endl;
+    cout << "exact: " << endl;
+    
+    print_float(exa,Nx,Ny);
+
+    cufftHandle transf;
+	cufftHandle inv_transf;
+	
+	cufft_error_func( cufftPlan2d( &transf, Ny, Nx, CUFFT_R2C ) );
+	cufft_error_func( cufftPlan2d( &inv_transf, Ny, Nx, CUFFT_C2R ) );
+
+    // cufft_error_func( cufftExecR2C(transf, test, htest) );
+    // cufft_error_func( cufftExecC2R(inv_transf, htest, test) );
+    FwdTrans(transf, test, htest, Nx, Ny);
+    BwdTrans(inv_transf, htest, test, Nx, Ny);
+    cuda_error_func( cudaPeekAtLastError() );
+	cuda_error_func( cudaDeviceSynchronize() );
+    cout<< endl;
+    cout << "after forward and inverse: " << endl;
+    print_float(test,Nx,Ny);
+
+    Dx<<<dimGrid,dimBlock>>>(htest, tmp, kx, Nxh, Ny);
+    // cuda_error_func( cudaPeekAtLastError() );
+	// cuda_error_func( cudaDeviceSynchronize() );
+
+    // cufft_error_func( cufftExecC2R(inv_transf, tmp, test) );
+    BwdTrans(inv_transf, tmp, test, Nx, Ny);
+    cuda_error_func( cudaPeekAtLastError() );
+	cuda_error_func( cudaDeviceSynchronize() );
+    cout<< endl;
+    cout << "Dx: " << endl;
+    print_float(test,Nx,Ny);
+
+    Dy<<<dimGrid,dimBlock>>>(htest, tmp, ky, Nxh, Ny);
+    // cuda_error_func( cudaPeekAtLastError() );
+	// cuda_error_func( cudaDeviceSynchronize() );
+    BwdTrans(inv_transf, tmp, test, Nx, Ny);
+
+    cuda_error_func( cudaPeekAtLastError() );
+	cuda_error_func( cudaDeviceSynchronize() );
+    cout<< endl;
+    cout << "Dy: " << endl;
+    print_float(test,Nx,Ny);
+
+    // cufft_error_func( cufftExecR2C(transf, test, htest) );
+    // cufft_error_func( cufftExecC2R(inv_transf, htest, test) );
+    FwdTrans(transf, test, htest, Nx, Ny);
+    BwdTrans(inv_transf, htest, test, Nx, Ny);
+    cuda_error_func( cudaPeekAtLastError() );
+	cuda_error_func( cudaDeviceSynchronize() );
+    cout<< endl;
+    cout << "after forward and inverse: " << endl;
+    print_float(test,Nx,Ny);
+    return 0;
+}
